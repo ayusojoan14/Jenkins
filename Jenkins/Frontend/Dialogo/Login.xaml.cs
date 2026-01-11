e783276e332c9fd8572922818fefdb112bab65fa
Joan <joanayuso@alu.edu.gva.es>
2026-01-11 19:56:29 +0100

    primer commit

---
+++ Jenkins/Frontend/Dialogo/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jenkins.Frontend.Dialogo
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : MetroWindow
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+     
+
+    private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí va la lógica de login
+            //Esta ya estaba
+            MainWindow ventanaPrincipal = new MainWindow();
+            ventanaPrincipal.Show();
+            this.Close();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí va la lógica de registro
+            MessageBox.Show("Register button clicked");
+        }
+
+    }
+}
---
+++ Jenkins/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Jenkins.Frontend.Dialogo;
+using MahApps.Metro.Controls;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jenkins
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnyadirReserva(object sender, MouseButtonEventArgs e) {
+            AnyiadirReserva ventana = new AnyiadirReserva(); 
+            ventana.Show();
+        }
+
+    }
+}
